d593d386334f4462b869ffd8422bebbec10c1710
sandroalme <111318914+sandroalme@users.noreply.github.com>
2023-06-28 17:55:16 -0300

    começando mais uma vez
    estava tudo dando errado, não estava entendendo nada, então decidi recomeçar

--- scripts.js
+++ scripts.js
@@ -1,41 +1,12 @@
-let salarioBruto = document.querySelector('.salario_bruto');
-let descontos = document.querySelector('.descontos');
-let dependentes = document.querySelector('.dependentes');
-let aliquotaInss = 0;
-
-const calcular = document.querySelector('.btn_calcular');
 const limpar = document.querySelector('.btn_limpar');
-const proventoBruto = document.querySelector('.tab_bruto');
-const total = document.querySelector('.total');
-const outros = document.querySelector('.outros');
-const campoAlInss = document.querySelector('.al_inss');
+const calcular = document.querySelector('.btn_calcular');
 
-function calcularSalario() {
-  
-  proventoBruto.textContent = salarioBruto.value;
-  outros.textContent = descontos.value;
-  total.textContent = salarioBruto.value;
-  campoAlInss.textContent = calcInss();
-  limparInputs();
-}
 
-function calcInss() {
-
-  if (salarioBruto <= 1320.00) {
-    return aliquotaInss = 7.5;
-  } else if (salarioBruto >= 1320.01 && salarioBruto <= 2571.29) {
-    return aliquotaInss = 9;
-  } else if (salarioBruto >= 2571.30 && salarioBruto <= 3856.94) {
-    return aliquotaInss = 12;
-  } else (salarioBruto >= 3856.95) {
-    return aliquotaInss = 14;
-  }
-}
 
 function limparInputs() {
-    salarioBruto.value = '';
-    descontos.value = '';
-    dependentes.value = '';
+  document.querySelector('.salario_bruto').value = "";
+  document.querySelector('.descontos').value = "";
+  document.querySelector('.dependentes').value = ""
 }
 
 calcular.addEventListener('click', (e) => {
